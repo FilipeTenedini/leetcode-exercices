1e20d86994977fa241cb4e76d85032bd8d7981e3
FilipeTenedini <tenedinifilipe@gmail.com>
2024-04-19 13:08:26 -0300

    feat: add two numbers

---
+++ resolved/add-two-numbers.go
@@ -0,0 +1,36 @@
+package resolved
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3 := &ListNode{Val: 0, Next: nil}
+	currentNode := l3
+	var carry = 0
+	for l1 != nil || l2 != nil || carry > 0 {
+		currentNode.Next = &ListNode{}
+		currentNode = currentNode.Next
+
+		var (
+			l1Value = 0
+			l2Value = 0
+		)
+
+		if l1 != nil {
+			l1Value = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2Value = l2.Val
+			l2 = l2.Next
+		}
+
+		sum := l1Value + l2Value + carry
+
+		currentNode.Val = sum % 10
+		carry = sum / 10
+	}
+	return l3.Next
+}
